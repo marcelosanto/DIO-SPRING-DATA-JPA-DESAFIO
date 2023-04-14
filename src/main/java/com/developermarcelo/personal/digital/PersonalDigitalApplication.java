e33f8198f05de3b994fa5ef5da4ef98bc47a64ac
Marcelo Santos <rocha.mer21@gmail.com>
2023-04-14 09:57:36 -0300

    initial commit

---
+++ src/main/java/com/developermarcelo/personal/digital/PersonalDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.developermarcelo.personal.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developermarcelo/personal/digital/PersonalDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developermarcelo.personal.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
